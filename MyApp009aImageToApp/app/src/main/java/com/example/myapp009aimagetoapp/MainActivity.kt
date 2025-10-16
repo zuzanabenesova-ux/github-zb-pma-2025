1ec2be441e115f9b2040e42c0d6fec420a1f9ff9
Uzivatel <>
2025-10-16 15:32:47 +0200

    dodělat

---
+++ MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapp009aimagetoapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getContent = registerForActivityResult(activityResultContracts.GetContent())
+            uri: Uri? -> binding.ivI
+
+        }
+    }
+}
