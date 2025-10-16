59e88278c0fab2a04d2946c6ca33a36cdf76d9e4
Uzivatel <>
2025-10-16 15:30:33 +0200

    hotovo

---
+++ MyApp007aFragments/app/src/main/java/com/example/myapp007afragments/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.myapp007afragments
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
+import com.example.myapp007afragments.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        // získání instance správce fragmentu
+        val fragmentManager = supportFragmentManager
+
+        // vyvoření nové transakce pro fragmenty
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // nahrazení fragmentu ve view kontejneru novým fragmentem, který byl předán jako argument
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+        // potvrzení transakce a provedení výměny fragmentů
+        fragmentTransaction.commit()
+    }
+}
---
+++ MyApp008aSharedPreferences/app/src/main/java/com/example/myapp008asharedpreferences/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapp008asharedpreferences
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp008asharedpreferences.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPreferences = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        binding.btnSave.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val ageString = binding.etAge.text.toString().trim()
+
+            if (ageString.isBlank()) {
+                Toast.makeText(this, "Musíš vyplnit věk", Toast.LENGTH_SHORT).show()
+            } else {
+                val age = ageString.toInt()
+                val isAdult = binding.cbAdult.isChecked
+                if ((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Kecáš", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Ukládám data...", Toast.LENGTH_SHORT).show()
+                    editor.apply {
+                        putString("name", name)
+                        putInt("age", age)
+                        putBoolean("isAdult", isAdult)
+                        apply()
+                    }
+                }
+            }
+            binding.btnLoad.setOnClickListener {
+                val name = sharedPreferences.getString("name", null)
+                val age = sharedPreferences.getInt( "age", 0)
+                val isAdult = sharedPreferences.getBoolean("isAdult", false)
+
+                binding.etName.setText(name)
+                binding.etName.setText(age.toString())
+                binding.cbAdult.isChecked = isAdult
+            }
+
+        }
+    }
+}
